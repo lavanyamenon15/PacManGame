2c089c5f7d896cf6d403b1c640da1fb57ed01539
Lavanya Menon <lavanyamenon1215@gmail.com>
2024-12-07 17:05:29 -0800

    debugged non-fiction related methods

--- src/App.java
+++ src/App.java
@@ -12,20 +12,24 @@ public class App {
     // parameter for each of our methods)
     static Scanner userInput = new Scanner(System.in); 
 
+    static void displaySeparator(){
+        System.out.println("\n************************************************************** \n" +
+        "************************************************************** \n");
+    }
+
     // EFFECTS: displays the welcome message for the user upon running the application
     static void displayWelcomeMessage(){
-        String introMessage = "\n************************************************************** \n" +
-        "************************************************************** \n\n" +
-        "Welcome to Reading Log! \nThe perfect place to keep track of everything you read and what you think about it :) \n"; 
+        displaySeparator();
+        String introMessage = "Welcome to Reading Log! \nThe perfect place to keep track of everything you read and what you think about it :) \n"; 
         System.out.println(introMessage);
     }
 
     // EFFECTS: displays the menu for the user and checks whether the input by the user is valid. Returns valid input.
     static int displayMenu(){
-        String menu = "What would you like to do? \n" +
-        "1. New entry \n" + 
-        "2. View your reading log \n" + 
-        "Please enter the number corresponding to the menu item you wish to choose"; 
+        String menu = "* What would you like to do? \n" +
+        "* 1. New entry \n" + 
+        "* 2. View your reading log \n" + 
+        "* Please enter the number corresponding to the menu item you wish to choose \n"; 
         System.out.println(menu);
 
         // this will keep looping until the user puts in a valid input (1 or 2) 
@@ -35,7 +39,6 @@ public class App {
                 userInput.nextLine();
 
                 if(menuSelection == 1 || menuSelection == 2){
-                    System.out.println("New Entry");
                     return menuSelection;
                 } else {
                     System.out.println("\n \n !! Try again: Please enter a number in the menu above !! \n \n");
@@ -53,7 +56,7 @@ public class App {
 
     // EFFECT: prompts the user for the title of the item they have read
     static String getTitle(String readingItemType){
-        String getTitleString = "\nWhat is the title of the " + readingItemType + "? \n";
+        String getTitleString = "\n* What is the title of the " + readingItemType + "? \n" ;
         System.out.println(getTitleString);
 
         while(true){
@@ -69,7 +72,7 @@ public class App {
 
     // EFFECT: prompts the user to enter the authors of the item they have read
     static ArrayList<Author> getAuthors(String readingItemType){
-        String howManyAuthors = "\nHow many authors are there for the " + readingItemType + "?";
+        String howManyAuthors = "\n* How many authors are there for the " + readingItemType + "? \n";
         System.out.println(howManyAuthors);
         ArrayList<Author> authors = new ArrayList<>();
         int numAuthors;
@@ -80,7 +83,6 @@ public class App {
                 break;
             } catch (Exception e) {
                 System.out.println("\n\n !! Try again: Please enter a number !! \n\n");
-                userInput.nextLine();
                 continue;
             }
         }
@@ -88,7 +90,7 @@ public class App {
         while(true) {
             try {
                 for(int i = 1; i <= numAuthors; i++) {
-                    System.out.println("\nWhat is the name of Author #" + i + "\n");
+                    System.out.println("\n* What is the name of Author #" + i + "\n");
                     String i_thAuthorName = userInput.nextLine();
                     authors.add(new Author(i_thAuthorName));
                 }
@@ -102,7 +104,7 @@ public class App {
 
     // EFFECT: prompts the user to enter the synopsis of the item they have read
     static String getSynopsis(String readingItemType){
-        String getSynopsisString = "\nWhat is the synopsis of the " + readingItemType + "? \n";
+        String getSynopsisString = "\n* What is the synopsis of the " + readingItemType + "? \n";
         System.out.println(getSynopsisString);
 
         while(true){
@@ -117,17 +119,16 @@ public class App {
     }
 
     // EFFECT: prompts the user to enter the date the item they have read was published
-    @SuppressWarnings("deprecation")
     static Date getDatePublished(String readingItemType){
-        String getDateString = "\nWhat is the Date the " + readingItemType + " was published? (please enter your answer in YYYY-MM-DD format)\n";
+        String getDateString = "\n* What is the Date the " + readingItemType + " was published? (please enter your answer in YYYY-MM-DD format) \n";
         System.out.println(getDateString);
 
         while(true){
             try {
                 String dateInput = userInput.nextLine();
-                int year = Integer.parseInt(dateInput.substring(0,4));
-                int month = Integer.parseInt(dateInput.substring(5,7));
-                int day = Integer.parseInt(dateInput.substring(8));
+                // int year = Integer.parseInt(dateInput.substring(0,4));
+                // int month = Integer.parseInt(dateInput.substring(5,7));
+                // int day = Integer.parseInt(dateInput.substring(8));
                 // userInput.nextLine();
                 // GregorianCalendar(year + 1900, month, day)
 
@@ -141,20 +142,20 @@ public class App {
 
     // EFFECT: prompts the user to enter the genre of the item they have read
     static ArrayList<Genre> getGenre(){
-        String getGenreString = "\n What are the genres of the book?" + 
-        "Please enter a list of numbers corresponding to the list below, separated by commas. (ex. 1,3,6,10) \n" +
-        "1. Fantasy\n" + 
-        "2. Historical Fiction\n" +
-        "3. Literary Fiction\n" +
-        "4. Drama\n" +
-        "5. Young Adult\n" +
-        "6. Classic\n" +
-        "7. Romance\n" +
-        "8. Thriller\n" +
-        "9. Comedy\n" +
-        "10. Poetry\n" +
-        "11. Mystery\n" +
-        "12. Science Fiction";
+        String getGenreString = "\n* What are the genres of the book? \n" + 
+        "* Please enter a list of numbers corresponding to the list below, separated by commas. (ex. 1,3,6,10) \n" +
+        "* 1. Fantasy\n" + 
+        "* 2. Historical Fiction\n" +
+        "* 3. Literary Fiction\n" +
+        "* 4. Drama\n" +
+        "* 5. Young Adult\n" +
+        "* 6. Classic\n" +
+        "* 7. Romance\n" +
+        "* 8. Thriller\n" +
+        "* 9. Comedy\n" +
+        "* 10. Poetry\n" +
+        "* 11. Mystery\n" +
+        "* 12. Science Fiction \n";
 
         System.out.println(getGenreString);
 
@@ -215,31 +216,31 @@ public class App {
 
     // EFFECT: prompts the user to enter the topic of the item they have read
     static ArrayList<Topic> getTopic(String readingItemType){
-        String getTopicString = "\n What are the topics of the " + readingItemType + "?" +  
-        "Please enter a list of numbers corresponding to the list below, separated by commas. (ex. 1,3,6,10) \n" +
-        "1. Self Help\n" + 
-        "2. Technology\n" +
-        "3. Philosophy\n" +
-        "4. Religion\n" +
-        "5. Politics\n" +
-        "6. International Relations\n" +
-        "7. Computer Science\n" +
-        "8. LLMs\n" +
-        "9. Algorithms\n" +
-        "10. Machine Learning\n" +
-        "11. AI\n" +
-        "12. Memoir\n" + 
-        "13. Biography\n" +
-        "14. Autobiography\n" +
-        "15. Sports\n" +
-        "16. Activism\n";
+        String getTopicString = "\n* What are the topics of the " + readingItemType + "? \n" +  
+        "* Please enter a list of numbers corresponding to the list below, separated by commas. (ex. 1,3,6,10) \n" +
+        "* 1. Self Help\n" + 
+        "* 2. Technology\n" +
+        "* 3. Philosophy\n" +
+        "* 4. Religion\n" +
+        "* 5. Politics\n" +
+        "* 6. International Relations\n" +
+        "* 7. Computer Science\n" +
+        "* 8. LLMs\n" +
+        "* 9. Algorithms\n" +
+        "* 10. Machine Learning\n" +
+        "* 11. AI\n" +
+        "* 12. Memoir\n" + 
+        "* 13. Biography\n" +
+        "* 14. Autobiography\n" +
+        "* 15. Sports\n" +
+        "* 16. Activism \n";
 
         System.out.println(getTopicString);
 
         while(true){
             try {
                 String topicInput = userInput.nextLine();
-                String[] topicList = topicInput.split(",");
+                String[] topicList = topicInput.split("[,]");
                 ArrayList<Topic> topicListEnums = new ArrayList<>();
                 for (String topic : topicList) {
                         switch (topic) {
@@ -295,7 +296,6 @@ public class App {
                                 break;
                     }
                 }
-                userInput.nextLine();
                 return topicListEnums;
             } catch (Exception e) {
                 userInput.nextLine();
@@ -306,13 +306,12 @@ public class App {
 
     // EFFECT: prompts the user to enter the DOI of the item they have read
     static String getDOI(){
-        String getDOIString = "\nWhat is the doi of the paper? \n";
+        String getDOIString = "\n* What is the doi of the paper?\n";
         System.out.println(getDOIString);
 
         while(true){
             try {
                 String DOIInput = userInput.nextLine();
-                userInput.nextLine();
                 return DOIInput;
             } catch (Exception e) {
                 userInput.nextLine();
@@ -324,13 +323,12 @@ public class App {
 
     // EFFECT: prompts the user to enter the journal of the item they have read
     static String getJournal(){
-        String getJournalString = "\nWhat is the journal that the paper was published in?\n";
+        String getJournalString = "\n* What is the journal that the paper was published in?\n";
         System.out.println(getJournalString);
 
         while(true){
             try {
                 String journalInput = userInput.nextLine();
-                userInput.nextLine();
                 return journalInput;
             } catch (Exception e) {
                 userInput.nextLine();
@@ -341,13 +339,12 @@ public class App {
 
     // EFFECT: prompts the user to enter the conference of the item they have read
     static String getConference(){
-        String getConferenceString = "\nWhat conference is associated with the paper?\n";
+        String getConferenceString = "\n* What conference is associated with the paper?\n";
         System.out.println(getConferenceString);
 
         while(true){
             try {
                 String conferenceInput = userInput.nextLine();
-                userInput.nextLine();
                 return conferenceInput;
             } catch (Exception e) {
                 userInput.nextLine();
@@ -358,7 +355,7 @@ public class App {
 
     // EFFECT: prompts the user to enter the news platform of the item they have read
     static String getNewsPlatform(){
-        String getNewsPlatformString = "\nWhat news platform was this article published on? \n";
+        String getNewsPlatformString = "\n* What news platform was this article published on? \n";
         System.out.println(getNewsPlatformString);
 
         while(true){
@@ -375,7 +372,7 @@ public class App {
 
     // EFFECT: prompts the user to enter the url of the item they have read
     static URL getURL(){
-        String getURL = "\nPlease enter the link for this article. \n";
+        String getURL = "\n* Please enter the link for this article. \n";
         System.out.println(getURL);
 
         while(true){
@@ -392,14 +389,14 @@ public class App {
 
     // EFFECTS: checks if the given reading items has the given attribute 
     static Boolean hasAttribute(String attributeName, String readingItemType){
-        System.out.println("Is there a " + attributeName + "associated with this " + readingItemType + "? (Answer with Y or N) \n");
+        System.out.println("\n* Is there a " + attributeName + " associated with this " + readingItemType + "? (Answer with Y or N) \n");
 
         while(true){
             try {
                 String hasAttribute = userInput.nextLine();
-                if(hasAttribute == "Y"){
+                if(hasAttribute.equals("Y")){
                     return true;
-                } else if (hasAttribute == "N") {
+                } else if (hasAttribute.equals("N")) {
                     return false;
                 } else {
                     System.out.println("\n\n !! Try again: Please answer with Y or N !! \n\n");
@@ -444,10 +441,10 @@ public class App {
 
     // EFFECT: prompts the user to select the type of nonfiction reading item they want to log 
     static String getTypeOfNonFiction() {
-        String typeOfNonFiction = "What type of non fiction reading item would you like to log? (please enter a number corresponding to an option below)\n" +
-        "1. Non Fiction Book\n"+
-        "2. Academic Paper\n" +
-        "3. Article\n";
+        String typeOfNonFiction = "\n* What type of non fiction reading item would you like to log? (please enter a number corresponding to an option below)\n" +
+        "* 1. Non Fiction Book\n"+
+        "* 2. Academic Paper\n" +
+        "* 3. Article\n";
         System.out.println(typeOfNonFiction);
 
         while(true){
@@ -511,9 +508,9 @@ public class App {
     // EFFECT: guides the user through selecting what reading item to created
     static ReadingItem getReadingItem(){
 
-        String bookTypeMenu = "What type of book did you read? (please enter a number corresponding to an option below): \n" +
-        "1. Fiction \n" +
-        "2. Non Fiction \n";
+        String bookTypeMenu = "\n* What type of book did you read? (please enter a number corresponding to an option below): \n" +
+        "* 1. Fiction \n" +
+        "* 2. Non Fiction \n";
 
         System.out.println(bookTypeMenu);
 
@@ -523,8 +520,10 @@ public class App {
                 userInput.nextLine();
                 switch (readingItemSelection) {
                     case 1:
+                        displaySeparator();
                         return getFictionBook();
                     case 2:
+                        displaySeparator();
                         return getNonFictionBook();
                     default:
                         System.out.println("\n \n !! Try again: Please enter a number in the menu above !! \n \n");
